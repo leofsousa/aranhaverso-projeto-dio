e6c786da7f0fcc74732eaafd9d34434b13eeeaf3
Leozera2k <leofsousa98@gmail.com>
2025-11-10 15:37:48 -0300

    framer motion instalado

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 import styles from "./heroesList.module.scss";
 import HeroPicture from "../HeroPicture";
+import { motion } from "framer-motion";
 
 interface IProps {
   heroes: IHeroData[];
@@ -10,16 +12,32 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+      <motion.h1
+        className={`${spidermanFont.className} ${styles.title}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 2 }}
+      >
         Personagens{" "}
-      </h1>
-      <section className={styles.heroes}>
+      </motion.h1>
+      <motion.section
+        className={styles.heroes}
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+      >
         {heroes.map((hero) => (
-          <div key={hero.id} className={`${styles.imageContainer} ${styles[hero.id]}`}>
+          <motion.div
+            key={hero.id}
+            className={`${styles.imageContainer} ${styles[hero.id]}`}
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.8 }}
+            transition={{ duration: 0.8 }}
+          >
             <HeroPicture hero={hero} />
-          </div>
+          </motion.div>
         ))}
-      </section>
+      </motion.section>
     </>
   );
 }
