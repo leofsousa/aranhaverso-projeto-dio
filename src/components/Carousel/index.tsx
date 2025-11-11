e0944e7f60f6786db854f36dd1df5c946c9579ae
Leozera2k <leofsousa98@gmail.com>
2025-11-11 12:40:23 -0300

    componente de informação implementado

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,10 +1,16 @@
 import { IHeroData } from "@/interfaces/heroes";
+import HeroDetails from "../HeroDetails";
 
 interface IProps {
   heroes: IHeroData[];
   activeId: string;
 }
 
-export default function Carousel({heroes, activeId}: IProps) {
-  return <h1>Componente Carousel: {activeId}</h1>
+export default function Carousel({ heroes, activeId }: IProps) {
+  return (
+    <>
+      <h1>Componente Carousel: {activeId}</h1>
+      <HeroDetails data={heroes[0]} />
+    </>
+  );
 }
