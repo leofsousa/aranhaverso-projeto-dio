19ae7f614d9e262f64e2b20be57912020c2b0fd1
Leozera2k <leofsousa98@gmail.com>
2025-11-17 11:17:34 -0300

    Update page.tsx

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -7,8 +7,13 @@ interface IProps {
   }>;
 }
 
-async function getHeroesData(): Promise<{data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+async function getHeroesData(): Promise<IHeroData[]> {
+  const res = await fetch(
+    "https://690a34181a446bb9cc21c572.mockapi.io/api/heroes",
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed to request heroes list");
@@ -21,6 +26,5 @@ export default async function Hero({ params }: IProps) {
   const { id } = await params;
   const heroes = await getHeroesData();
 
-  return <Carousel heroes={heroes.data} activeId={id}/>;
+  return <Carousel heroes={heroes} activeId={id} />;
 }
-
